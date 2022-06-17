e39f0992b39d01c66b9916418ac0efe73e389b15
shree <s.r.vitkar55@gmail.com>
2022-06-17 13:33:13 +0530

    host allow

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-sf@dow2py@9hxl6hp3us*&4wqhv5%%^!g%)q=92i0#wv+a$k&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'my-pro-app.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
